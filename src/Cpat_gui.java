a2f06f05e2bb5d40f53928425cd9c6a09ece6f3f
jaspe <Jasper_versantvoort@hotmail.com>
2020-11-06 16:14:47 +0100

    Commentaar/java docs toegevoegd

--- src/Cpat_gui.java
+++ src/Cpat_gui.java
@@ -22,9 +22,14 @@ public class Cpat_gui {
     private final JPanel tekenpanel;
 
     public static void main(String[] args) {
+        /**
+         * main functie van deze code
+         * Het frame wordt gemaakt en gepakt
+         */
         JFrame frame = new JFrame("Cpat_gui");
         frame.setContentPane(new Cpat_gui().panel1);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        // Grote ingesteld en er wordt gezorgd dat je het frame niet kan veranderen.
         frame.setPreferredSize(new Dimension(700, 600));
         frame.setResizable(false);
         frame.pack();
@@ -32,6 +37,13 @@ public class Cpat_gui {
     }
 
     public Cpat_gui() {
+        /**
+         * @functie de files worden gelezen, file1 kijk per regel of de protein code in file 2 zit.
+         * Wanneer dit zo is wordt dit opgeslagen
+         * uiteindelijke worden de overlappingen, en de unieke regels gevisualiseerd met behulp van ovalen.
+         * @exception wanneer het bestand niet gevonden kan worden wordt er een fileNotFoundException gegeven
+         * @exception wanneer een protein code niet begint met 'AT' wordt er een NotValidProteinCode exception gegeven
+         */
         tekenpanel = new JPanel();
         tekenpanel.setPreferredSize(new Dimension(600, 200));
         tekenpanel.setBackground(Color.white);
@@ -40,6 +52,9 @@ public class Cpat_gui {
         blader1Button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                /**
+                 * @functie Laat je een bestand kiezen deze wordt in geefBestandTextField geplaatst
+                 */
                 File selectedFile;
                 JFileChooser fileChooser = new JFileChooser();
                 int reply = fileChooser.showOpenDialog(null);
@@ -53,6 +68,9 @@ public class Cpat_gui {
         blader2Button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                /**
+                 * @functie Laat je een bestand kiezen deze wordt in geefBestandTextField1 geplaatst
+                 */
                 File selectedFile;
                 JFileChooser fileChooser = new JFileChooser();
                 int reply = fileChooser.showOpenDialog(null);
@@ -66,6 +84,12 @@ public class Cpat_gui {
         analyseerHetConsensusProteoomButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                /**
+                 * @functie de aantal overeenkomende en unieke protein codes worden berekend.
+                 * Daarna wordt dit gevisualiseerd
+                 * @exception wanneer het bestand niet gevonden kan worden wordt er een fileNotFoundException gegeven
+                 * @exception wanneer een protein code niet begint met 'AT' wordt er een NotValidProteinCode exception gegeven
+                 */
                 String line;
                 String line2;
                 int gelijk = 0;
@@ -134,8 +158,8 @@ public class Cpat_gui {
                     tekenveld.drawString("# Overlap in bestanden", 250, 170);
                     tekenveld.drawString(Integer.toString(gelijk), 300, 100);
                 } catch (IOException fileNotFoundException) {
-                    System.out.println("bestand niet gevonden, probeer opnieuw");
-                    textArea1.setText("bestand niet gevonden, probeer opnieuw");
+                    JOptionPane.showMessageDialog(null,
+                            "bestand(en) niet gevonden probeer opnieuw");
                 } catch (NotValidProteinCode er) {
                     JOptionPane.showMessageDialog(null, er.getMessage());
                 }
